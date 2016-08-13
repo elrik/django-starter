1949966dcefa928973575b9d711108e588572447
Nikola Trandafilovic <nikola@elegantsolutions.no>
2016-08-13 14:08:43 +0200

    Added basic project structure

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/urls.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/urls.py
@@ -1,4 +1,4 @@
-"""my_cookiecutter_stuff URL Configuration
+"""{{cookiecutter.project_name}} URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
